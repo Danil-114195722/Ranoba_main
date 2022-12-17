35fdf5283e48bce66cddfa77d03d9d7ac871fcfe
Ej_you <dudkovdy@gmail.com>
2022-12-17 14:10:48 +0300

    try again

--- entrance/views.py
+++ entrance/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def login(request):
+    return render(request, 'entrance/login.html')
+
+
+def registration(request):
+    return render(request, 'entrance/registration.html')
+
+
+def politics(request):
+    return render(request, 'entrance/politics.html')
